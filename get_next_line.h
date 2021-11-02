fcc0af69697f7e87150636b06d959e2516b79a41
AOhapkin <alexeyyago@gmail.com>
2021-11-02 17:37:08 +0300

    add utils

--- get_next_line.h
+++ get_next_line.h
@@ -10,8 +10,7 @@ char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *str, int c);
-char	*ft_newstr(size_t size);
-void	*ft_memalloc(size_t size);
-void	*ft_memset(void *dest, int c, size_t count);
+char	*ft_substr(char const *str, unsigned int start, size_t len);
+char	*ft_strdup(const char *str);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -51,37 +51,20 @@ char	*ft_strchr(const char *str, int c)
 	return (NULL);
 }
 
-char	*ft_newstr(size_t size)
+char	*ft_strdup(const char *str)
 {
-	char	*dest;
-
-	dest = malloc(size + 1);
-	if (!dest)
-		return (NULL);
-	return ((char *)ft_memset(dest, 0, size));
-}
-
-void	*ft_memalloc(size_t size)
-{
-	void	*dest;
-
-	dest = malloc(size);
-	if (!dest)
-		return (NULL);
-	return (ft_memset(dest, 0, size));
-}
-
-void	*ft_memset(void *dest, int c, size_t count)
-{
-	size_t			i;
-	unsigned char	*ptr;
+	char	*dup;
+	int		i;
 
 	i = 0;
-	ptr = (unsigned char *)dest;
-	while (i < count)
+	dup = malloc(sizeof(char) * (ft_strlen(str) + 1));
+	if (!dup)
+		return (NULL);
+	while (str[i])
 	{
-		ptr[i] = (unsigned char)c;
+		dup[i] = str[i];
 		i++;
 	}
-	return (dest);
+	dup[i] = '\0';
+	return (dup);
 }
